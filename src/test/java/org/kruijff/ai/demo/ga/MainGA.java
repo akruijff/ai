bb9b44d4fc571f83f5ea256c25ffe47d80523223
Alex de Kruijff <akruijff@dds.nl>
2019-07-31 20:49:59 +0200

    Move the two demo applications into their own package

--- src/test/java/org/kruijff/ai/demo/ga/MainGA.java
+++ src/test/java/org/kruijff/ai/demo/ga/MainGA.java
@@ -26,7 +26,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package org.kruijff.ai.demo;
+package org.kruijff.ai.demo.ga;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
